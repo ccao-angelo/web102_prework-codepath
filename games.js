b7699d66f0276ddda2f1b2ab57d3e36fcf1e4891
Chau Cao <cnhc3615@gmail.com>
2026-01-07 00:17:01 -0800

    commit

--- games.js
+++ games.js
@@ -1,5 +1,5 @@
 // Image credits: Kickstarter
-const games = ` [
+const GAMES = [
   {
     "name": "Heroes Of Mythic Americas",
     "description": "An exciting 5e RPG supplement that heroically represents pre-Columbian American cultures and mythologies",
@@ -89,16 +89,5 @@ const games = ` [
     "img": "./assets/kingdom_death.png"
   }
 ]
-`
-const template = `
-{
-"name": "",
-"description": "",
-"pledged": 0,
-"goal": 0,
-"backers": 0,
-"img": ""
-},
-`
 
-export default games;
+export default GAMES;
